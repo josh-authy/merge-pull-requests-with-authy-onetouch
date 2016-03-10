023b44c549d0cea504f715463c55ae1ea809adeb
Cesar Pino <cesarpino547@gmail.com>
2016-03-09 16:07:23 -0800

    link non clickable

--- api/services/onetouch.js
+++ api/services/onetouch.js
@@ -6,13 +6,13 @@ module.exports = {
                     -d api_key="' + process.env.AUTHY_API_KEY + '" \
                     -d message="Pull Request - ' + pr.pull_request.base.repo.name + '" \
                     -d details[title]="' + pr.pull_request.title + '" \
+                    -d details[body]="' + pr.pull_request.body + '" \
+                    -d details[username]="' + pr.pull_request.user.login + '" \
                     -d details[from]="' + pr.pull_request.head.label + '" \
                     -d details[to]="' + pr.pull_request.base.label + '" \
-                    -d details[link]="<a href=\"' + pr.pull_request.html_url + '\">' + pr.pull_request.html_url + '</a>" \
+                    -d details[link]="' + pr.pull_request.html_url + '" \
                     -d seconds_to_expire=0';
-    // -d details[body]="' + pr.pull_request.body + '" \
-    // -d details[username]="' + pr.pull_request.user.login + '" \
-                    
+
     return commands.run(command);
   }
 };
